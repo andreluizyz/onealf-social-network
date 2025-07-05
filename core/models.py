5a685f6c30cd21d23226637f6bc58636827f6f74
André Luiz <alffexbr@gmail.com>
2025-07-04 21:28:18 -0300

    Alterações

--- core/models.py
+++ core/models.py
@@ -29,5 +29,14 @@ class Comentario(models.Model):
     def __str__(self):
         return f'Comentário de {self.autor.usuario.username} em "{self.post.titulo}"'
 
-# class Curtida(models.Model):
+class Curtida(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='curtidas')
+    perfil = models.ForeignKey(Perfil, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('post', 'perfil')  
+
+    def __str__(self):
+        return f'{self.perfil.usuario.username} curtiu "{self.post.titulo}"'
+
 
